1e6599e9571239e7514e7e90676e4ce6754aac57
lisacatalano <lcatdesigns@gmail.com>
2025-02-05 13:34:42 -0600

    Set up Router

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,25 @@
 import { createRouter, createWebHistory } from 'vue-router';
+import HomeView from '@/views/HomeView.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: [
+    {
+      path: '/',
+      name: 'Home',
+      component: HomeView,
+    },
+    {
+      path: '/projects',
+      name: 'Projects',
+      component: () => import('@/views/ProjectsView.vue'),
+    },
+    {
+      path: '/projects/:id',
+      name: 'Project',
+      component: () => import('@/views/SingleProjectView.vue'),
+    },
+  ],
 });
 
 export default router;
