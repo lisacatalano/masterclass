dd017d826e9ee04caa8e2ab2c36c50666225cd84
lisacatalano <lcatdesigns@gmail.com>
2025-02-12 15:50:50 -0600

    Set up Shad componenets and tailwind

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,18 @@ import { fileURLToPath, URL } from 'node:url';
 import VueRouter from 'unplugin-vue-router/vite';
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
+import autoprefixer from 'autoprefixer';
+import tailwind from 'tailwindcss';
 import vueDevTools from 'vite-plugin-vue-devtools';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [VueRouter(), vue(), vueDevTools()],
+  css: {
+    postcss: {
+      plugins: [tailwind(), autoprefixer()],
+    },
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
