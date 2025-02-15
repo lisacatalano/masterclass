b32d858e68d5c7a105ef7e96a285e0a2b4160aaa
lisacatalano <lcatdesigns@gmail.com>
2025-02-15 15:48:59 -0600

    Create layouts for app

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,9 @@
 <script setup lang="ts">
-import TopNavBar from '@/components/layout/TopNavBar.vue';
-import Sidebar from '@/components/layout/Sidebar.vue';
+import AuthLayout from '@/components/layout/main/AuthLayout.vue';
 </script>
 
 <template>
-  <Sidebar />
-  <div class="flex flex-col lg:ml-52 ml-16 transition-[margin]">
-    <TopNavBar />
-    <main>
-      <RouterView />
-    </main>
-  </div>
+  <AuthLayout>
+    <RouterView />
+  </AuthLayout>
 </template>
