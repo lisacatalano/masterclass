82dda9598dc8f1836c2ffe5b77fcc964ed7682ac
lisacatalano <lcatdesigns@gmail.com>
2025-02-17 18:11:43 -0600

    Add unplugin-auto-import

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,9 @@ import vue from '@vitejs/plugin-vue';
 import autoprefixer from 'autoprefixer';
 import tailwind from 'tailwindcss';
 import vueDevTools from 'vite-plugin-vue-devtools';
+// vite.config.ts
+import AutoImport from 'unplugin-auto-import/vite';
+import { VueRouterAutoImports } from 'unplugin-vue-router';
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -16,6 +19,23 @@ export default defineConfig({
           isCustomElement: (element) => element.startsWith('iconify-icon'),
         },
       },
+    }),
+    AutoImport({
+      // targets to transform
+      include: [
+        /\.[tj]sx?$/, // .ts, .tsx, .js, .jsx
+        /\.vue$/,
+        /\.vue\?vue/, // .vue
+        /\.md$/, // .md
+      ],
+      // global imports to register
+      imports: [
+        // presets
+        'vue',
+        VueRouterAutoImports,
+      ],
+      dts: true,
+      viteOptimizeDeps: true,
     }),
     vueDevTools(),
   ],
